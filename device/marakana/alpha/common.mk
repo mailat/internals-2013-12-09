c38fe01e5d53cbe1862bb3d06760a4143073cccf
Marius Mailat <marius.mailat@appseleration.com>
2013-12-10 21:02:31 +0200

    + rift names for marakana init.rc

--- device/marakana/alpha/common.mk
+++ device/marakana/alpha/common.mk
@@ -22,8 +22,8 @@ include $(MY_PATH)/packages.mk
 
 # Copy our init and ueventd configuration files to the root
 # file system (ramdisk.img -> boot.img)
-PRODUCT_COPY_FILES += $(MY_PATH)/init.marakanaalphaboard.rc:root/init.marakanaalphaboard.rc
-PRODUCT_COPY_FILES += $(MY_PATH)/ueventd.marakanaalphaboard.rc:root/ueventd.marakanaalphaboard.rc
+PRODUCT_COPY_FILES += $(MY_PATH)/init.goldfish.rc:root/init.goldfish.rc
+PRODUCT_COPY_FILES += $(MY_PATH)/ueventd.goldfish.rc:root/ueventd.goldfish.rc
 
 # Enable our custom kernel
 LOCAL_KERNEL := $(MY_PATH)/kernel
