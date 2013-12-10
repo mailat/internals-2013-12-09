c071026ed7187c84e246e572f6eeb7e478e503de
Marius Mailat <marius.mailat@appseleration.com>
2013-12-10 12:05:52 +0200

    + first day with custom device + overlay for resources

--- README.md
+++
@@ -1,4 +0,0 @@
-internals-2013-12-09
-====================
-
-Android Internals Intel
---
+++ device/marakana/alpha/full_marakana_alpha.mk
@@ -0,0 +1,18 @@
+# Get the long list of APNs
+#PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Discard inherited values and use our own instead
+PRODUCT_NAME := full_marakana_alpha
+PRODUCT_DEVICE := alpha
+PRODUCT_MODEL := Full Marakana Alpha Image for Emulator
+PRODUCT_BRAND := Android_Marakana
+PRODUCT_MANUFACTURER := Marakana
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/marakana/alpha/device.mk)
+
+# Include the common definitions and packages
+include $(LOCAL_PATH)/common.mk
